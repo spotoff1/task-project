a6414aa70058e484b668a136315c4bc0998f0b53
spotoff1 <damnspotoff@gmail.com>
2026-09-24 14:59:15 +0300

    test: add status filter test

--- tests/test_task_app.py
+++ tests/test_task_app.py
@@ -54,3 +54,21 @@ class TestCompleteTask(unittest.TestCase):
         with open(self.tmp.name, encoding='utf-8') as f:
             tasks = json.load(f)
         self.assertEqual(tasks[0]['status'], 'done')
+
+class TestFilterByStatus(unittest.TestCase):
+    def setUp(self):
+        self.tmp = tempfile.NamedTemporaryFile(delete=False, suffix='.json')
+        self.tmp.close()
+        task_app.DATA_FILE = self.tmp.name
+        task_app.add_task('Открытая')
+        task_app.add_task('Закрытая')
+        task_app.complete_task(2)
+
+    def tearDown(self):
+        os.unlink(self.tmp.name)
+
+    def test_filter_open(self):
+        tasks = task_app.load_tasks()
+        opened = [t for t in tasks if t['status'] == 'open']
+        self.assertEqual(len(opened), 1)
+        self.assertEqual(opened[0]['title'], 'Открытая')
